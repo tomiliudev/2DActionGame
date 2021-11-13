85daebbf1be72bbe93177c74348bad75c8294ec2
tomiliudev <tomiliudev@yahoo.co.jp>
2021-11-14 00:57:06 +0900

    Stage2シーンの作成とスライムの仮実装

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -5,6 +5,7 @@ public class Enemy : MonoBehaviour
     [SerializeField] protected SpriteRenderer sr;
     [SerializeField] protected Rigidbody2D rb2D;
     [SerializeField] protected CircleCollider2D cc2D;
+    [SerializeField] protected CapsuleCollider2D capsuleCollider2D;
     [SerializeField] protected float moveSpeed;
     [SerializeField] protected float gravity;
     [SerializeField] protected EnemyCollisionCheck ecc;
@@ -20,7 +21,8 @@ public class Enemy : MonoBehaviour
         {
             isDead = true;
             rb2D.velocity = new Vector2(0f, -3f);
-            cc2D.enabled = false;
+            if (cc2D != null) cc2D.enabled = false;
+            if (capsuleCollider2D != null) capsuleCollider2D.enabled = false;
             if (animator != null) animator.enabled = false;
         }
         return isDead;
---
+++ Assets/Scripts/Enemy/SlimeEnemy.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using UnityEngine;
+
+public class SlimeEnemy : Enemy
+{
+    private bool isRight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Move());
+    }
+
+    /// <summary>
+    /// プレイヤーを追従する
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator Move()
+    {
+        yield return new WaitForEndOfFrame();
+        while (true)
+        {
+            yield return new WaitForFixedUpdate();
+
+            // 敵が死んだら何もしない
+            if (base.IsEnemyDead()) yield break;
+
+            // ゲームクリアしたら何もしない
+            if (base.IsGameClear)
+            {
+                base.rb2D.Sleep();
+                yield break;
+            }
+
+            if (base.sr.isVisible)
+            {
+                if (base.ecc != null && base.ecc.IsOn)
+                {
+                    isRight = !isRight;
+                }
+
+                float xVector = -1f;
+                if (isRight)
+                {
+                    xVector = 1f;
+                    transform.localScale = new Vector3(-1f, 1f, 1f);
+                }
+                else
+                {
+                    transform.localScale = new Vector3(1f, 1f, 1f);
+                }
+                rb2D.velocity = new Vector2(xVector * moveSpeed, -gravity);
+            }
+            else
+            {
+                rb2D.Sleep();
+            }
+        }
+    }
+}
