9bb25ccfccefc49ba8797154d19ed3cfc8ff0c26
tomiliudev <tomiliudev@yahoo.co.jp>
2021-11-20 23:06:30 +0900

    Platformのロジック変更とStage2のマップ作成

--- Assets/Scripts/Gimmicks/Platform.cs
+++ Assets/Scripts/Gimmicks/Platform.cs
@@ -1,13 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
 public class Platform : MonoBehaviour
 {
-    [SerializeField] PolygonCollider2D cameraArea;
+    [SerializeField] e_MoveType moveType;
+    [SerializeField] float moveSpeed = 3f;
+
+    private enum e_MoveType
+    {
+        up,
+        down,
+        left,
+        right
+    }
 
     private Vector2 localPosition;
+    
+    private float cameraColliderUpY;
+    private float cameraColliderDownY;
 
     // Start is called before the first frame update
     void Start()
@@ -23,13 +33,45 @@ public class Platform : MonoBehaviour
 
     private void FixedUpdate()
     {
-        if (transform.localPosition.y < cameraArea.points.ElementAt(2).y)
+        Vector2 direction = Vector2.up;
+        switch (moveType)
         {
-            transform.localPosition = new Vector2 (localPosition.x, 4.5f);
+            case e_MoveType.up:
+                direction = transform.up;
+                break;
+            case e_MoveType.down:
+                direction = -transform.up;
+                break;
+            case e_MoveType.left:
+                break;
+            case e_MoveType.right:
+                break;
         }
-        else
+
+        transform.Translate(direction * moveSpeed * Time.deltaTime);
+    }
+
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.tag == "CameraCollider")
         {
-            transform.Translate(-transform.up * 3f * Time.deltaTime);
+            var cameraCollider = collision.GetComponent<PolygonCollider2D>();
+            cameraColliderUpY = cameraCollider.points.ElementAt(1).y;
+            cameraColliderDownY = cameraCollider.points.ElementAt(2).y;
+            switch (moveType)
+            {
+                case e_MoveType.up:
+                    transform.localPosition = new Vector2(localPosition.x, cameraColliderDownY);
+                    break;
+                case e_MoveType.down:
+                    transform.localPosition = new Vector2(localPosition.x, cameraColliderUpY);
+                    break;
+                case e_MoveType.left:
+                    break;
+                case e_MoveType.right:
+                    break;
+            }
         }
     }
 }
