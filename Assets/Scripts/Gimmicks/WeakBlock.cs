695db381bab91159d2974a9b34fc5278a1fa4ed2
tomiliudev <tomiliudev@yahoo.co.jp>
2021-12-03 13:25:29 +0900

    WeakBlock作業中一旦コミット

--- Assets/Scripts/Gimmicks/WeakBlock.cs
+++ Assets/Scripts/Gimmicks/WeakBlock.cs
@@ -1,14 +1,33 @@
+using System.Collections;
 using System.Linq;
 using UnityEngine;
 
 public class WeakBlock : MonoBehaviour
 {
-
+    [SerializeField] Rigidbody2D rg2d;
+    [SerializeField] Animator animator;
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.collider.tag == "Player")
+        switch (collision.collider.tag)
         {
-            Debug.Log("Player Enter!");
+            case "Player":
+                StartCoroutine(CrushAnimation());
+                break;
+            case "Ground":
+                animator.SetTrigger("crush");
+                Destroy(gameObject);
+                break;
         }
+    }
+
+    IEnumerator CrushAnimation()
+    {
+        
+        float shackTime = 1.5f;
+        gameObject.ShakePosition(new Vector2(0.1f, 0f), shackTime, 0f);
+
+        yield return new WaitForSeconds(shackTime);
+
+        rg2d.velocity = new Vector2(0f, -3f);
     }
 }
