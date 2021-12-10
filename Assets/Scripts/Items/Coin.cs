8d96627eb658cf11b6fdf140a1b1e87339b2c0b0
tomiliudev <tomiliudev@yahoo.co.jp>
2021-12-10 23:45:32 +0900

    コインを取った時のスコアを表示できるようにしました

--- Assets/Scripts/Items/Coin.cs
+++ Assets/Scripts/Items/Coin.cs
@@ -2,10 +2,12 @@ using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
+    [SerializeField] GameObject score;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Player")
         {
+            score.SetActive(true);
             Destroy(gameObject);
         }
     }
---
+++ Assets/Scripts/Others/Score.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        transform.parent = null;
+        var _canvas = GetComponent<Canvas>();
+        _canvas.enabled = true;
+        float yPos = _canvas.transform.position.y;
+
+        Hashtable hash = new Hashtable();
+        hash.Add("y", yPos + 1.5f);
+        hash.Add("time", 0.5f);
+        hash.Add("oncomplete", "OnComplete");
+        iTween.MoveTo(gameObject, hash);
+    }
+
+    private void OnComplete()
+    {
+        Destroy(gameObject);
+    }
+}
