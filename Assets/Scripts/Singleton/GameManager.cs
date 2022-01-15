2295ff3848958d864aefe6deaf05610e1730756e
tomiliudev <tomiliudev@yahoo.co.jp>
2022-01-16 01:52:52 +0900

    ちょっと修正

--- Assets/Scripts/Singleton/GameManager.cs
+++ Assets/Scripts/Singleton/GameManager.cs
@@ -52,7 +52,7 @@ public class GameManager : SingletonMonoBehaviour<GameManager>
 
     private void Start()
     {
-        currentStage = e_StageName.Stage3;
+        currentStage = e_StageName.Stage1;
         LoadSceneTo(currentStage.ToString());
     }
 
