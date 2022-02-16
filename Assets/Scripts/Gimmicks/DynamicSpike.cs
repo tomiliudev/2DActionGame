122e8dff1d65d76899a0d032813e32cbcb578821
tomiliudev <tomiliudev@yahoo.co.jp>
2022-02-16 21:31:56 +0900

    DynamicSpikeロジック修正

--- Assets/Scripts/Gimmicks/DynamicSpike.cs
+++ Assets/Scripts/Gimmicks/DynamicSpike.cs
@@ -10,19 +10,100 @@ public sealed class DynamicSpike : MonoBehaviour
         right
     }
     [SerializeField] Direction direction;
+    [SerializeField] float distance = 2f;
+    [SerializeField] bool isTorigger;
 
+
+    GameManager gm;
+    RaycastHit2D hit;
+    int PlayerLayerMask;
+
+    Vector2 originPos;
     float originYpos;
     float originXpos;
+    Vector3 originAngle;
+    BoxCollider2D boxCo2d;
+
+    Vector2 _boundsTopLeftCorner;
+    Vector2 _boundsTopRightCorner;
+    Vector2 _boundsBottomLeftCorner;
+    Vector2 _boundsBottomRightCorner;
     private void Start()
     {
+        gm = GameManager.Instance;
+
+        PlayerLayerMask = 1 << LayerMask.NameToLayer("Player");
+
+        originPos = transform.position;
         originXpos = transform.position.x;
         originYpos = transform.position.y;
-        
-        UpAnime();
+        originAngle = transform.localEulerAngles;
+
+        Debug.Log(originAngle);
+
+        if (isTorigger)
+        {
+            // トリガーで発動
+            SetRaycastInfo();
+        }
+        else
+        {
+            // 自動発動
+            UpAnime();
+        }
     }
 
+    private void Update()
+    {
+        CheckTorigger();
+    }
+
+    private void SetRaycastInfo()
+    {
+        boxCo2d = GetComponent<BoxCollider2D>();
+        float top = boxCo2d.offset.y + (boxCo2d.size.y / 2f);
+        float bottom = boxCo2d.offset.y - (boxCo2d.size.y / 2f);
+        float left = boxCo2d.offset.x - (boxCo2d.size.x / 2f);
+        float right = boxCo2d.offset.x + (boxCo2d.size.x / 2f);
+        _boundsTopLeftCorner.x = left;
+        _boundsTopLeftCorner.y = top;
+        _boundsTopRightCorner.x = right;
+        _boundsTopRightCorner.y = top;
+        _boundsBottomLeftCorner.x = left;
+        _boundsBottomLeftCorner.y = bottom;
+        _boundsBottomRightCorner.x = right;
+        _boundsBottomRightCorner.y = bottom;
+        _boundsTopLeftCorner = transform.TransformPoint(_boundsTopLeftCorner);
+        _boundsTopRightCorner = transform.TransformPoint(_boundsTopRightCorner);
+        _boundsBottomLeftCorner = transform.TransformPoint(_boundsBottomLeftCorner);
+        _boundsBottomRightCorner = transform.TransformPoint(_boundsBottomRightCorner);
+
+        _boundsTopLeftCorner += new Vector2(0f, 0.5f);
+        _boundsTopRightCorner += new Vector2(0f, 0.5f);
+        _boundsBottomLeftCorner += new Vector2(0f, -0.5f);
+        _boundsBottomRightCorner += new Vector2(0f, -0.5f);
+    }
+
+    private void CheckTorigger()
+    {
+        if (!isTorigger) return;
+
+        Vector2 dir = (Vector2)gm.player.transform.position - originPos;
+        hit = Physics2D.Raycast(originPos, dir, distance, PlayerLayerMask);
+        Debug.DrawRay(originPos, dir * distance, Color.red);
+
+        if (hit.collider != null && LayerMask.LayerToName(hit.collider.gameObject.layer) == "Player")
+        {
+            if (!isAniming) UpAnime();
+        }
+    }
+
+    // アニメーション中
+    bool isAniming;
     private void UpAnime()
     {
+        isAniming = true;
+
         float moveToX = originXpos + 1;
         float moveToY = originYpos + 1f;
         switch (direction)
@@ -36,16 +117,15 @@ public sealed class DynamicSpike : MonoBehaviour
                 moveToY = originYpos - 1f;
                 break;
             case Direction.left:
-                moveToX = originXpos + 1;
+                moveToX = originXpos - 1;
                 moveToY = originYpos + 0f;
                 break;
             case Direction.right:
-                moveToX = originXpos - 1;
+                moveToX = originXpos + 1;
                 moveToY = originYpos + 0f;
                 break;
         }
 
-        
         iTween.MoveTo(gameObject,
             iTween.Hash(
                 "x", moveToX,
@@ -77,6 +157,11 @@ public sealed class DynamicSpike : MonoBehaviour
 
     private void OnDownAnimeComplete()
     {
-        Invoke("UpAnime", 2f);
+        isAniming = false;
+
+        if (!isTorigger)
+        {
+            Invoke("UpAnime", 2f);
+        }
     }
 }
